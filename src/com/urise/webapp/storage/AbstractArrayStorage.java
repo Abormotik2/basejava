1d20c3d1d38e6706e7ac376aee13f5da97890141
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-11 16:47:56 +0300

    HW3 abstract storage & sorted storage

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,10 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10_000;
+
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size;
+}
