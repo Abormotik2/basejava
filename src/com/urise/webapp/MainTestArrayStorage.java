686b0c2989e29b1e098bca8ada223ca08c230ec8
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-13 15:47:38 +0300

    HW3 implements interface
    change update resume

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -25,7 +25,7 @@ public class MainTestArrayStorage {
         Resume r4 = new Resume();
         r4.setUuid("uuid4");
 
-        ARRAY_STORAGE.update(r4);
+        ARRAY_STORAGE.update(r3);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
