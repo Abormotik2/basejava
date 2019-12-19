e4a1afc13ed5ca0f216f6897d1f8fe6d70340c3b
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-19 23:40:57 +0300

    HW04 Reflection and JUnit

--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -2,7 +2,7 @@ package com.urise.webapp.storage;
 
 public class SortedArrayStorageTest extends  AbstractArrayStorageTest {
 
-    SortedArrayStorageTest(Storage storage) {
-        super(storage);
+   public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
     }
 }
