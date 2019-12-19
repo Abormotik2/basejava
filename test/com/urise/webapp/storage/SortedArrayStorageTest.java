217b794dc09461a555a05f1ce08ce2257a2731e3
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-19 22:06:53 +0300

    Create SortedArrayStorageTest.java

---
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,8 @@
+package com.urise.webapp.storage;
+
+public class SortedArrayStorageTest extends  AbstractArrayStorageTest {
+
+    SortedArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+}
