100396a6a5422d6699486fcf664f8ada6d75b734
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-19 22:06:55 +0300

    Create ArrayStorageTest.java

---
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,8 @@
+package com.urise.webapp.storage;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+    protected ArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+}
