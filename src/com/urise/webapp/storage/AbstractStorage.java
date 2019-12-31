9457ab06bd44725f677f327d0212a2ed8e9eda7b
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-31 14:21:48 +0300

    Lesson05 Static initializer block and Collections

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -1,5 +1,17 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.model.Resume;
+
 public abstract class AbstractStorage implements Storage {
 
+    public abstract void clear();
+    protected abstract Integer notExistResume(String uuid);
+    protected abstract Integer existResume(String uuid);
+    public abstract void update(Resume resume);
+    public abstract void save(Resume resume);
+    public abstract Resume get(String uuid);
+    public abstract void delete(String uuid);
+    public abstract Resume[] getAll();
+    public abstract int size();
+    protected abstract Integer getIndex(String uuid);
 }
