669ce9cf84e480527f1757e389a6893024ba4c69
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-31 14:21:34 +0300

    HW04 Exceptions

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,5 +1,7 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
 /**
@@ -7,20 +9,36 @@ import com.urise.webapp.model.Resume;
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    protected int getIndex(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     protected void saveResume(Resume resume, int sIndex) {
         storage[size] = resume;
     }
 
     protected void deleteResume(int dIndex) {
         storage[dIndex] = storage[size - 1];
+    }
+
+    protected Integer getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    protected Integer existResume(String uuid) {
+        Integer existIndex = getIndex(uuid);
+        if (existIndex != null) {
+            throw new ExistStorageException(uuid);
+        }
+        return existIndex;
+    }
+
+    protected Integer notExistResume(String uuid) {
+        Integer notExistIndex = getIndex(uuid);
+        if (notExistIndex == null) {
+            throw new NotExistStorageException(uuid);
+        }
+        return notExistIndex;
     }
 }
