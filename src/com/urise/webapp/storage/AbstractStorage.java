46c2c76a961f464b0ac2251e590f5a58c2fbaea4
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-04 22:37:00 +0300

    Lesson05 Static initializer block and Collections

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -4,14 +4,37 @@ import com.urise.webapp.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
 
-    public abstract void clear();
-    protected abstract Integer notExistResume(String uuid);
-    protected abstract Integer existResume(String uuid);
-    public abstract void update(Resume resume);
-    public abstract void save(Resume resume);
-    public abstract Resume get(String uuid);
-    public abstract void delete(String uuid);
-    public abstract Resume[] getAll();
-    public abstract int size();
+    protected abstract Object notExistResume(String uuid);
+
+    protected abstract Object existResume(String uuid);
+
     protected abstract Integer getIndex(String uuid);
+
+    protected abstract void updateResume(Object upIndex, Resume resume);
+
+    protected abstract void saveResume(Resume resume);
+
+    protected abstract Resume getResume(Object gIndex, String uuid);
+
+    protected abstract void deleteResume(Object dIndex, String uuid);
+
+    public void update(Resume resume) {
+        Object upIndex = notExistResume(resume.getUuid());
+        updateResume(upIndex, resume);
+    }
+
+    public void save(Resume resume) {
+        existResume(resume.getUuid());
+        saveResume(resume);
+    }
+
+    public Resume get(String uuid) {
+        Object gIndex = notExistResume(uuid);
+        return getResume(gIndex, uuid);
+    }
+
+    public void delete(String uuid) {
+        Object dIndex = notExistResume(uuid);
+        deleteResume(dIndex, uuid);
+    }
 }
