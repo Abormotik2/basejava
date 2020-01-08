7ac74f52f17f40ee307c4b193267f5c5f51780cd
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-08 18:09:23 +0300

    Lesson05 Static initializer block and Collections

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -4,6 +4,7 @@ import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,23 +18,23 @@ public class MapStorage extends AbstractStorage {
     }
 
     @Override
-    public void updateResume(Object upIndex, Resume resume) {
-        mapResumes.put((String) upIndex, resume);
+    public void updateResume(Object index, Resume resume) {
+        mapResumes.put((String) index, resume);
     }
 
     @Override
-    public void saveResume(Resume resume) {
+    public void saveResume(Resume resume, Object index) {
         mapResumes.put(resume.getUuid(), resume);
     }
 
     @Override
-    public Resume getResume(Object gIndex) {
-        return mapResumes.get(gIndex);
+    public Resume getResume(Object index) {
+        return mapResumes.get((String)index);
     }
 
     @Override
-    public void deleteResume(Object dIndex) {
-        mapResumes.remove(dIndex);
+    public void deleteResume(Object index) {
+        mapResumes.remove((String)index);
     }
 
     @Override
@@ -48,12 +49,15 @@ public class MapStorage extends AbstractStorage {
 
     @Override
     public Integer getIndex(String uuid) {
+        if (mapResumes.containsKey(uuid)) {
+            return new ArrayList<>(mapResumes.entrySet()).indexOf(uuid);
+        }
         return null;
     }
 
     @Override
     protected String existResume(String uuid) {
-        if (mapResumes.containsKey(uuid)) {
+        if (validIndex(uuid)) {
             throw new ExistStorageException(uuid);
         }
         return uuid;
@@ -61,9 +65,14 @@ public class MapStorage extends AbstractStorage {
 
     @Override
     protected String notExistResume(String uuid) {
-        if (!mapResumes.containsKey(uuid)) {
+        if (!validIndex(uuid)) {
             throw new NotExistStorageException(uuid);
         }
         return uuid;
+    }
+
+    @Override
+    protected boolean validIndex(String uuid) {
+        return getIndex(uuid) != null;
     }
 }
