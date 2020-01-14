3786a8a96f79cc1d99e36a3ce9412a3065cd6fa4
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-14 20:48:28 +0300

    Lesson06 Iterator, Comparator and Inner Classes
    add the method getAllsorted for real. sort in son methods, add getALL(abstract) for real. in sons classes.

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -4,6 +4,9 @@ import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
+import java.util.Collections;
+import java.util.List;
+
 public abstract class AbstractStorage implements Storage {
 
     protected abstract Object getSearchKey(String uuid);
@@ -17,6 +20,8 @@ public abstract class AbstractStorage implements Storage {
     protected abstract void remove(Object searchKey);
 
     protected abstract boolean isValid(Object searchKey);
+
+    protected abstract List<Resume> getAll();
 
     public void update(Resume resume) {
         Object searchKey = notExistResume(resume.getUuid());
@@ -44,6 +49,12 @@ public abstract class AbstractStorage implements Storage {
             throw new ExistStorageException(uuid);
         }
         return key;
+    }
+
+    public List<Resume> getAllSorted() {
+        List<Resume> list = getAll();
+        Collections.sort(list);
+        return list;
     }
 
     private Object notExistResume(String uuid) {
