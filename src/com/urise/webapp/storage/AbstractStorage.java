1c858fd3b69aed783f16df5b34bce24c6955720e
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-15 12:40:02 +0300

    Lesson06 Iterator, Comparator and Inner Classes
    add Comparator for realized sort

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -4,11 +4,12 @@ import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class AbstractStorage implements Storage {
 
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid).thenComparing(Resume::getFullName);
     protected abstract Object getSearchKey(String uuid);
 
     protected abstract void refresh(Resume resume, Object searchKey);
@@ -53,7 +54,7 @@ public abstract class AbstractStorage implements Storage {
 
     public List<Resume> getAllSorted() {
         List<Resume> list = getAll();
-        Collections.sort(list);
+        list.sort(RESUME_COMPARATOR);
         return list;
     }
 
