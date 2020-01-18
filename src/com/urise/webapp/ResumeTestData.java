dd68f163d366ddb1b615f9dc5a04ffe1fb86b026
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-18 23:00:36 +0300

    HW07
    put resume

--- src/com/urise/webapp/ResumeTestData.java
+++ src/com/urise/webapp/ResumeTestData.java
@@ -1,11 +1,25 @@
 package com.urise.webapp;
 
+import com.urise.webapp.model.ContactType;
 import com.urise.webapp.model.Resume;
+
+import java.util.Map;
+
+import static com.urise.webapp.model.ContactType.*;
 
 public class ResumeTestData {
 
     public static void main(String[] args) {
-        Resume resume1 = new Resume("Григорий Кислин");
-
+        Resume resume = new Resume("Григорий Кислин");
+        Map<ContactType, String> contactInfo = resume.getContacts();
+        contactInfo.put(MOBILE, "+7(921) 855-0482");
+        contactInfo.put(SKYPE,  "grigory.kislin");
+        contactInfo.put(MAIL, "gkislin@yandex.ru");
+        contactInfo.put(LINKEDIN, "https://www.linkedin.com/in/gkislin");
+        contactInfo.put(GITHUB,"https://github.com/gkislin");
+        contactInfo.put(STACKOVERFLOW, "https://stackoverflow.com/users/548473");
+        contactInfo.put(HOME_PAGE, "http://gkislin.ru/");
+        List<>
+        System.out.println(resume);
     }
 }
