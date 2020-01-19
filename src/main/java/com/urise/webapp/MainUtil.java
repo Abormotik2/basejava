fb9fd2e257de6911c390d66963488f3685746a85
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-19 19:48:03 +0300

    Lesson06 HW05

---
+++ src/main/java/com/urise/webapp/MainUtil.java
@@ -0,0 +1,19 @@
+package com.urise.webapp;
+
+public class MainUtil {
+    public static void main(String[] args) {
+        System.out.println(Integer.valueOf(-1) == Integer.valueOf(-1));
+        System.out.println(Integer.valueOf(-1) == new Integer(-1));
+        int result = getInt();
+        System.out.println(result);
+        Integer a1 = 50;
+        Integer a2 = 50;
+        Integer b1 = 500;
+        Integer b2 = 500;
+        System.out.println(a1==a2);
+        System.out.println(b1==b2);
+    }
+    private static Integer getInt() {
+        return -1;
+    }
+}
