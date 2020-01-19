fe76de55f8d6f202cb455c67895ea9a16c912245
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-19 19:52:06 +0300

    Lesson07_HW06 add fullName

---
+++ src/main/java/com/urise/webapp/model/Resume.java
@@ -0,0 +1,70 @@
+package com.urise.webapp.model;
+
+import java.util.*;
+
+/**
+ * Initial resume class
+ */
+public class Resume  {
+    // Unique identifier
+    private final String uuid;
+
+    private final String fullName;
+
+    private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
+
+    private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
+
+    public Resume(String fullName) {
+        this(UUID.randomUUID().toString(), fullName);
+    }
+
+    public Resume(String uuid, String fullName) {
+        Objects.requireNonNull(uuid, "Uuid must not be null");
+        Objects.requireNonNull(fullName, "fullName must not be null");
+        this.uuid = uuid;
+        this.fullName = fullName;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Map<ContactType, String> getContacts() {
+        return contacts;
+    }
+
+    public Map<SectionType, Section> getSections() {
+        return sections;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        if (!uuid.equals(resume.uuid)) return false;
+        return fullName.equals(resume.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = uuid.hashCode();
+        result = 31 * result + fullName.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Resume{" +
+                "uuid='" + uuid + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", contacts=" + contacts +
+                ", sections=" + sections +
+                '}';
+    }
+}
