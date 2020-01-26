95050e689040ccd91b41a6bf2200a4b386de706c
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-26 22:41:58 +0300

    Lesson07_HW06 add fullName

--- src/main/java/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/main/java/com/urise/webapp/storage/SortedArrayStorage.java
@@ -10,14 +10,14 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
 
     @Override
-    protected void doSave(Resume resume, Integer index) {
+    protected void insert(Resume resume, Integer index) {
         int varIndex = index * -1 - 1;
         if (size - varIndex >= 0) System.arraycopy(storage, varIndex, storage, varIndex + 1, size - varIndex);
         storage[varIndex] = resume;
     }
 
     @Override
-    protected void doDelete(Integer index) {
+    protected void remove(Integer index) {
         System.arraycopy(storage, index + 1, storage, index, size - index - 1);
     }
 
