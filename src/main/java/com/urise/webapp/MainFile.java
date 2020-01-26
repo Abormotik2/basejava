3153bf5801b977a3de766e1df53e6b0acbd8e6d3
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-01-26 22:39:31 +0300

    Lesson08 File and Resources
    do recourse

--- src/main/java/com/urise/webapp/MainFile.java
+++ src/main/java/com/urise/webapp/MainFile.java
@@ -7,16 +7,24 @@ import java.nio.file.Paths;
 
 public class MainFile {
     public static void main(String[] args) {
-        printCatalog(Paths.get("E:/Java/basejava/src/main/java"));
+        System.out.println(printCatalog(Paths.get("E:/Java/basejava/src/main/java")));
     }
 
-    public static void printCatalog(Path catalogPath) {
+    public static String printCatalog(Path catalogPath) {
+        StringBuilder builder = new StringBuilder();
         try {
+            builder.append(catalogPath.getFileName()).append('\n');
             Files
-                    .walk(catalogPath)
-                    .forEach(path -> System.out.println(path.getFileName()));
+                    .list(catalogPath)
+                    .filter(Files::isDirectory)
+                    .forEach(path -> builder.append('\t').append(printCatalog(path)).append('\n'));
+            Files
+                    .list(catalogPath)
+                    .filter(Files::isRegularFile)
+                    .forEach(path -> builder.append('\t').append('\t').append("--").append(path.getFileName()).append('\n'));
         } catch (IOException e) {
             throw new RuntimeException("Error", e);
         }
+        return builder.toString();
     }
 }
