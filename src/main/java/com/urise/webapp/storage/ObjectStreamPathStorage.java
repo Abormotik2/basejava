770028be841b4e8b07e511eb7e28d3fd4f068896
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-02-05 00:40:29 +0300

    HW09 delete constractor
    ,made implements Strategy

--- src/main/java/com/urise/webapp/storage/ObjectStreamPathStorage.java
+++ src/main/java/com/urise/webapp/storage/ObjectStreamPathStorage.java
@@ -5,22 +5,15 @@ import com.urise.webapp.model.Resume;
 
 import java.io.*;
 
-public class ObjectStreamPathStorage extends AbstractPathStorage {
+public class ObjectStreamPathStorage implements StrategySerialized {
 
-
-    protected ObjectStreamPathStorage(String dir) {
-        super(dir);
-    }
-
-    @Override
-    protected void doWrite(Resume resume, OutputStream os) throws IOException {
+    public void doWrite(Resume resume, OutputStream os) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(os)) {
             oos.writeObject(resume);
         }
     }
 
-    @Override
-    protected Resume doRead(InputStream is) throws IOException {
+    public Resume doRead(InputStream is) throws IOException {
         try (ObjectInputStream ois = new ObjectInputStream(is)) {
             return (Resume) ois.readObject();
         } catch (ClassNotFoundException e) {
