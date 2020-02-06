cf9beda646d8ca96c9a3b44278a0c08552d3dcdf
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-02-06 22:42:04 +0300

    HW09 add new classes //transport strategy into new directory
    add new classes

--- test/java/com/urise/webapp/storage/AllStorageTest.java
+++ test/java/com/urise/webapp/storage/AllStorageTest.java
@@ -4,7 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class, SortedArrayStorageTest.class, ListStorageTest.class, MapUuidStorageTest.class, MapResumeStorageTest.class
+@Suite.SuiteClasses({ArrayStorageTest.class
+        , SortedArrayStorageTest.class
+        , ListStorageTest.class
+        , MapUuidStorageTest.class
+        , MapResumeStorageTest.class
+        , ObjectStreamStorageTest.class
+        , ObjectStreamPathStorageTest.class
 })
 public class AllStorageTest {
 }
