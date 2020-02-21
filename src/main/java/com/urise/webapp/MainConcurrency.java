5e22d4b83069849d2345d3ea0832aaecd6a27675
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2020-02-21 21:21:49 +0300

    HW12 Cuncurency

--- src/main/java/com/urise/webapp/MainConcurrency.java
+++ src/main/java/com/urise/webapp/MainConcurrency.java
@@ -1,21 +1,33 @@
 package com.urise.webapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
-    private volatile int counter;
-    private static final Object LOCK = new Object();
+    private int counter;
+    private AtomicInteger atomicCount = new AtomicInteger();
 
-    public static void main(String[] args) {
+    //    private static final Object LOCK = new Object();
+//    private static final Lock lock = new ReentrantLock();
+    private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
+    private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = ThreadLocal.withInitial(SimpleDateFormat::new);
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
 
         Thread thread0 = new Thread() {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
+//                throw new IllegalStateException();
             }
         };
         thread0.start();
@@ -38,18 +50,29 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
+//
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
 
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+
+            Future<Integer> future = executorService.submit(() ->
+//            Thread thread = new Thread(() ->
+            {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
+                    System.out.println(threadLocal.get().format(new Date()));
                 }
+                latch.countDown();
+                return 5;
             });
-            thread.start();
-            threads.add(thread);
+//            thread.start();
+//            threads.add(thread);
         }
 
+/*
         threads.forEach(t -> {
             try {
                 t.join();
@@ -57,88 +80,49 @@ public class MainConcurrency {
                 e.printStackTrace();
             }
         });
-        System.out.println(mainConcurrency.counter);
+*/
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+//        System.out.println(mainConcurrency.counter);
+        System.out.println(mainConcurrency.atomicCount.get());
 
-        final Resource1 resource1 = new Resource1();
-        final Resource2 resource2 = new Resource2();
-        resource1.resource2 = resource2;
-        resource2.resource1 = resource1;
-        final Thread1 thread1 = new Thread1();
-        final Thread2 thread2 = new Thread2();
-        thread1.resource1 = resource1;
-        thread2.resource2 = resource2;
-        thread1.start();
-        thread2.start();
-        final Object dLock1 = new Object();
-        final Object dLock2 = new Object();
-        deadLock(dLock1, dLock2);
-        deadLock(dLock2, dLock2);
+//        final String lock1 = "lock1";
+//        final String lock2 = "lock2";
+//        deadLock(lock1, lock2);
+//        deadLock(lock2, lock1);
     }
+//
+//    private static void deadLock(Object lock1, Object lock2) {
+//        new Thread(() -> {
+//            System.out.println("Waiting " + lock1);
+//            synchronized (lock1) {
+//                System.out.println("Holding " + lock1);
+//                try {
+//                    Thread.sleep(50);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("Waiting " + lock2);
+//                synchronized (lock2) {
+//                    System.out.println("Holding " + lock2);
+//                }
+//            }
+//        }).start();
+//    }
 
     private void inc() {
 //        synchronized (this) {
 //        synchronized (MainConcurrency.class) {
-        counter++;
+//        WRITE_LOCK.lock();
+//        try {
+        atomicCount.incrementAndGet();
+//            counter++;
+//        } finally {
+//            WRITE_LOCK.unlock();
+//        }
 //                wait();
 //                readFile
 //                ...
 //        }
-    }
-
-    private static class Thread1 extends Thread {
-        Resource1 resource1;
-
-        @Override
-        public void run() {
-            System.out.println(resource1.getI());
-        }
-    }
-
-    private static class Thread2 extends Thread {
-        Resource2 resource2;
-
-        @Override
-        public void run() {
-            System.out.println(resource2.getI());
-        }
-    }
-
-    private static class Resource1 {
-        Resource2 resource2;
-
-        public synchronized int getI() {
-            return resource2.returnI();
-        }
-
-        public synchronized int returnI() {
-            return 2;
-        }
-    }
-
-    private static class Resource2 {
-        Resource1 resource1;
-
-        public synchronized int getI() {
-            return resource1.returnI();
-        }
-
-        public synchronized int returnI() {
-            return 2;
-        }
-    }
-    private static void deadLock(Object dLock1, Object dLock2) {
-        new Thread(() -> {
-            synchronized (dLock1) {
-                System.out.println(dLock1);
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    throw new IllegalStateException(e);
-                }
-                synchronized (dLock2) {
-                    System.out.println(dLock2);
-                }
-            }
-        }).start();
     }
 }
